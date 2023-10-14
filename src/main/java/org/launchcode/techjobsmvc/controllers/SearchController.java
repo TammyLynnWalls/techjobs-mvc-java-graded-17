6ec9e90d18f82135e979c8c63f027729ced18637
Tammy <tammylwalls@gmail.com>
2023-10-13 23:37:53 -0500

    added the displaySearchResults handler and the table to the search.html, BUT the radio buttons disappear when results display and resubmitting breaks the page

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,9 +1,15 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.Job;
+import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -22,6 +28,19 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping("/results")
+    public String displaySearchResults(Model model, @RequestParam String searchType,  @RequestParam String searchTerm){
+        ArrayList<Job> jobs = new ArrayList<>();
+        if (searchType.equals("all") || searchTerm.equals("") || searchTerm.equals("all")){
+            jobs = JobData.findAll();
+        }else{
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("jobs", jobs);
+        return "/search";
+
+        //TODO FOR TAM: make it so the search radio buttons show and can continue resubmitting the form
+    }
 
 }
 
