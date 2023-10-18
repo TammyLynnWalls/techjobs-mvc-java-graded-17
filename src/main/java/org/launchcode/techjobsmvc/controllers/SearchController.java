257a2b8fc33b0bda141445eae2c8403dfac3930c
Tammy <tammylwalls@gmail.com>
2023-10-17 22:45:04 -0500

    got the radio buttons showing with search results, got the page titles displaying correctly.  Still need the "All" results to show the correct title.  The "All" link says Jobs with Null.  Also, the displaySearchResults() has the title hard coded and should be dynamic.

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -28,18 +28,25 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+
+    //
     @PostMapping("/results")
     public String displaySearchResults(Model model, @RequestParam String searchType,  @RequestParam String searchTerm){
         ArrayList<Job> jobs = new ArrayList<>();
         if (searchType.equals("all") || searchTerm.equals("") || searchTerm.equals("all")){
-            jobs = JobData.findAll();
+           jobs = JobData.findAll();
+//           model.addAttribute("title", "All Jobs" );
+            model.addAttribute("title", "Jobs With All:");
         }else{
             jobs = JobData.findByColumnAndValue(searchType, searchTerm);
         }
+        model.addAttribute("columns", columnChoices);
         model.addAttribute("jobs", jobs);
+        model.addAttribute("title", "Jobs With "+ columnChoices.get(searchType)+": "+ searchTerm);
         return "/search";
 
-        //TODO FOR TAM: make it so the search radio buttons show and can continue resubmitting the form
+        //TODO FOR TAM: make it so the page title displays the column.value
+        //TODO FOR TAM: make it so the ALL link says "ALL Jobs"
     }
 
 }
